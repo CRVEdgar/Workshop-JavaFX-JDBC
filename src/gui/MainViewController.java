53d6efd01d5963bf39cc9bf67b0fdf85748661e8
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-03-07 19:17:49 -0300

    DepartmentList View created

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+CONTROLADOR DA TELA PRINCIPAL
+
  */
 package gui;
 
@@ -49,7 +48,7 @@ public class MainViewController implements Initializable {
     
     @FXML
     public void onMenuItemDepartmentAction(){
-        System.out.println("onMenuItemDepartmentAction");
+        loadView("/gui/DepartmentList.fxml");
     }
     
     @FXML
